9f0d1a460d0e70710849c90f851201daaf179514
roman-jordan <rjordan1352@gmail.com>
2019-12-06 22:00:09 -0800

    update

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -4,14 +4,15 @@ import GithubSignInButton from './GithubSignInButton'
 import GoogleSignInButton from './GoogleSignInButton'
 import FacebookSignInButton from './FacebookSignInButton'
 import LocalSignUpForm from './LocalSignUpForm'
+import './signIn.scss'
 const SignInCanvas = () =>{
     console.log('herehere')
     return(
         <div className="SignInCanvas">
-            <GithubSignInButton/><br />
+            {/* <GithubSignInButton/><br /> */}
             <GoogleSignInButton/><br />
             <FacebookSignInButton/><br />
-            <LocalSignUpForm />
+            {/* <LocalSignUpForm /> */}
         </div>
     );
 }
